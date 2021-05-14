50922de04d07110a6c709f654e73d8dbc6ccfe67
MasonPLong <ml858315@ohio.edu>
2021-05-13 22:46:23 -0400

    Initial commit

---
+++ git-lab-program.cpp
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cpp
+ *      Author: Mason Long
+ *        Date: 5-13-2021
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
